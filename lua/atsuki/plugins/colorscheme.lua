9368fb7ec7486c61886f0fe9f9616588635abc9c
atsuki <csatsfapsfu2930@gmail.com>
2023-09-18 23:20:23 -0700

    added trasnparency and organized file

--- lua/atsuki/plugins/colorscheme.lua
+++ lua/atsuki/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
+			vim.g.nightflyTransparent = true
 			vim.cmd([[colorscheme nightfly]])
 		end,
 	},
